3cc56907f356f002ee764540c2586db7e4fc61e1
Unknown <sarah4594@gmail.com>
2019-04-24 20:10:37 -0400

    Add welcome component and render with props

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const welcome = "Welcome to React";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -10,23 +8,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome />
+          <Welcome text="Welcome to React From Props" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
 class Welcome extends Component {
   render() {
-    return (
-      <h1 className="App-title">{welcome}</h1>
-    )
+    const { text } = this.props
+    return <h1 className="App-title">{text}</h1>
   }
 }
 
-
-export default App;
+export default App
