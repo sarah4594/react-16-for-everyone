85fc8cdff48891716be2a1405a7617aa7f38b002
Unknown <sarah4594@gmail.com>
2019-04-28 12:41:46 -0500

    Add controlled input handler

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -7,6 +7,16 @@ class App extends Component {
     console.log(this.text.value)
   }
 
+  updateInput = event => {
+    this.setState({
+      input: event.target.value.trim()
+    })
+  }
+
+  state = {
+    input: 'Hello'
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,6 +27,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>{this.state.input}</h3>
+        <input
+          type="text"
+          onChange={this.updateInput}
+          value={this.state.input}
+        />
         <input type="text" ref={input => (this.text = input)} />
         <button onClick={this.submit}>Show Value</button>
       </div>
