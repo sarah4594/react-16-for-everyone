790a2a9b25ba277488edf60a426c4bf180849f21
Unknown <sarah4594@gmail.com>
2019-04-28 13:33:48 -0500

    Render movie list

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Movie from './Movie'
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider Man'
+  },
+  {
+    id: 3,
+    title: 'Avengers Endgame'
+  },
+  {
+    id: 4,
+    title: 'Captain Marvel'
+  }
+]
 
 class App extends Component {
   render() {
@@ -9,6 +29,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     )
   }
