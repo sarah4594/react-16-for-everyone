d2ba9ca52eab579d8580c819674b285f4542f6f0
Unknown <sarah4594@gmail.com>
2019-05-04 09:57:27 -0500

    Add test router path

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 import Movie from './Movie'
@@ -25,16 +26,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Route path="/test" component={Test} />
+          {this.state.movies.map(movie => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </Router>
     )
   }
 }
 
 export default App
+
+const Test = () => <h1>TEST</h1>
