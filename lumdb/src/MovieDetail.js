005cd0355f1d07f4b8becaf90d6eb6fc0d7f7112
Unknown <sarah4594@gmail.com>
2019-05-06 20:39:27 -0500

    Add movie details

---
+++ lumdb/src/MovieDetail.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280'
+
+class MovieDetail extends Component {
+  static propTypes = {
+    match: PropTypes.object
+  }
+
+  state = {
+    movie: {}
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }?api_key=98492a20dd76cdfcc8ecd4440636c231&language=en-US`
+      )
+      const movie = await res.json()
+      this.setState({
+        movie
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    const { movie } = this.state
+    return (
+      <div>
+        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <h3>{movie.release_date}</h3>
+        <p>{movie.overview}</p>
+      </div>
+    )
+  }
+}
+
+export default MovieDetail
