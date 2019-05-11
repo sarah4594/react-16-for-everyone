1d72263ae0e7738eaa8f33dd60995f610345e4e7
Unknown <sarah4594@gmail.com>
2019-05-10 20:31:16 -0500

    Change MovieList to a pure component

--- lumdb/src/MovieList.js
+++ lumdb/src/MovieList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 import Movie from './Movie'
 
-class MovieList extends Component {
+class MovieList extends PureComponent {
   state = {
     movies: []
   }
