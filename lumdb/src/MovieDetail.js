58ef8d5d65c83985050002deafb40d812d8a7d12
Unknown <sarah4594@gmail.com>
2019-05-13 18:17:11 -0500

    Add conditional render

--- lumdb/src/MovieDetail.js
+++ lumdb/src/MovieDetail.js
@@ -34,6 +34,7 @@ class MovieDetail extends Component {
 
   render() {
     const { movie } = this.state
+
     return (
       <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
         <MovieInfo>
@@ -44,6 +45,7 @@ class MovieDetail extends Component {
             />
           </Overdrive>
           <div>
+            {movie.title ? <h1>Hello</h1> : <h1>Hi</h1>}
             <h1>{movie.title}</h1>
             <h3>{movie.release_date}</h3>
             <p>{movie.overview}</p>
